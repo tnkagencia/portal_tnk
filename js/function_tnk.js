d7fc32fc7d1051e1e29ba1244c416cde9ddd0c74
tnkagencia <rfelix@tnkagencia.com.br>
2017-08-16 17:56:08 -0300

    201708161
    Ajustes fim do dia 16/08/2017

---
+++ js/function_tnk.js
@@ -0,0 +1,22 @@
+<script LANGUAGE="JavaScript">
+d = new Date();
+hour = d.getHours();
+if(hour < 5){
+    document.write("Boa Noite");
+    }
+else
+if(hour < 8){
+    document.write("Bom Dia");
+    }
+else
+if(hour < 12){
+    document.write("Bom Dia!");
+    }
+else
+if(hour < 18){
+    document.write("Boa tarde");
+    }
+else{
+    document.write("Boa noite");
+    }
+</script>
